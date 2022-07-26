03b8edb90b4be1d7812c16e0656e4147b3f76d5c
Dmitriy Panteleev <dpanteleev@tradingview.com>
2022-07-26 10:02:37 +0300

    biuty

--- mathdoku/main.py
+++ mathdoku/main.py
@@ -7,8 +7,8 @@ import CalculatorGui
 class CalculatorApp(QtWidgets.QMainWindow, CalculatorGui.Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
-        self.calcButton.clicked.connect(self.button_pushed)  # Выполнить функцию browse_folder при нажатии кнопки
+        self.setupUi(self)
+        self.calcButton.clicked.connect(self.button_pushed)
 
     def button_pushed(self):
         self.listWidget.addItem(str(self.checkBox_1.isChecked()))
@@ -19,5 +19,5 @@ def main():
     window.show()  # Показываем окно
     app.exec_()  # и запускаем приложение
 
-if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
-    main()  # то запускаем функцию main()
+if __name__ == '__main__':
+    main()
