d5c08bf1bd5261e83d284e12d8695768a8febbea
Dmitriy Panteleev <dpanteleev@tradingview.com>
2022-10-06 09:41:16 +0300

    add ignore

--- func-drawer/simple-drawer-ti.py
+++ func-drawer/simple-drawer-ti.py
@@ -8,41 +8,51 @@ ti.init()
 sizewidth = 1000 # size of pictures in pixels
 sizeheight = 1000 # size of pictures in pixels
 
-# real area of drawing
-xmin = -100
-xmax = 100
-xlen = abs((xmax-xmin) / sizewidth)
-ymin = -100
-ymax = 100
-ylen = abs((ymax-ymin) / sizeheight)
-
-def mfunc(x,y,exec_formule):
-    # formula of function
-    return x**2 + y**2 - 2500
-
 image = Image.new("RGB", (sizewidth, sizeheight))
 draw = ImageDraw.Draw(image)
 
-xr = xmin
-yr = ymin
-f1 = mfunc(xr,yr,formule)
-f2 = mfunc(xr,yr + ylen,formule)
-f3 = mfunc(xr + xlen,yr,formule)
-f4 = mfunc(xr + xlen,yr + ylen,formule)
+@ti.func
+def mfunc(x,y):
+    # formula of function
+    return x**2 + y**2 - 2500
+@ti.kernel
+def calc():
+    sizewidth = 1000 # size of pictures in pixels
+    sizeheight = 1000 # size of pictures in pixels
 
-for x in range(sizewidth):
-    xr = xmin + x * xlen
-    for y in range(sizeheight):
-        yr = ymin + y * ylen
+    # real area of drawing
+    xmin = -100
+    xmax = 100
+    xlen = abs((xmax-xmin) / sizewidth)
+    ymin = -100
+    ymax = 100
+    ylen = abs((ymax-ymin) / sizeheight)
 
-        f2 = mfunc(xr,yr + ylen,formule)
-        f4 = mfunc(xr + xlen,yr + ylen,formule)
+    xr = xmin
+    yr = ymin
+    f1 = mfunc(xr,yr)
+    f2 = mfunc(xr,yr + ylen)
+    f3 = mfunc(xr + xlen,yr)
+    f4 = mfunc(xr + xlen,yr + ylen)
 
-        if (f1 > 0 and f2 > 0 and f3 > 0 and f3 > 0) or (f1 < 0 and f2 < 0 and f3 < 0 and f3 < 0) :
-            continue
-        draw.point((x, y), fill=ImageColor.getrgb("red"))
+    for x in range(sizewidth):
+        xr = xmin + x * xlen
+        for y in range(sizeheight):
+            yr = ymin + y * ylen
 
-        f1 = f2
-        f3 = f4
+            f2 = mfunc(xr,yr + ylen)
+            f4 = mfunc(xr + xlen,yr + ylen)
 
-image.save("/home/dpanteleev/SomeStuff/mybox/func-drawer/empty.png", "PNG")
+            if (f1 > 0 and f2 > 0 and f3 > 0 and f3 > 0) or (f1 < 0 and f2 < 0 and f3 < 0 and f3 < 0) :
+                continue
+            # draw.point((x, y), fill=ImageColor.getrgb("red"))
+
+            f1 = f2
+            f3 = f4
+
+    # image.save("/home/dpanteleev/SomeStuff/mybox/func-drawer/empty.png", "PNG")
+
+tic = time.perf_counter()
+calc()
+toc = time.perf_counter()
+print(f"Execution time {toc - tic:0.4f} seconds")
