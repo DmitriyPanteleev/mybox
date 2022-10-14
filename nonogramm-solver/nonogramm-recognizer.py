9e14074d92ccc6388c8a9f01df956b1d913eea21
Dmitriy Panteleev <dpanteleev@tradingview.com>
2022-10-14 11:15:16 +0300

    easyocr version

--- nonogramm-solver/nonogramm-recognizer.py
+++ nonogramm-solver/nonogramm-recognizer.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
 import easyocr
+from pprint import pprint
 
-im_1_path = './test2.png'
+im_1_path = '/home/dpanteleev/SomeStuff/mybox/nonogramm-solver/test2.png'
 
 def recognize_text(img_path):
     '''loads an image and recognizes text.'''
@@ -11,4 +12,4 @@ def recognize_text(img_path):
     return reader.readtext(img_path)
 
 result = recognize_text(im_1_path)
-print(result)
+pprint(result)
