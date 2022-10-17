e35a29d970208df39040255f4bbbe937eb03c8d9
Dmitriy Panteleev <dpanteleev@tradingview.com>
2022-10-17 14:21:25 +0300

    mostly well working version

--- nonogramm-solver/nonogramm-recognizer.py
+++ nonogramm-solver/nonogramm-recognizer.py
@@ -1,9 +1,9 @@
 # import the necessary packages
-from pytesseract import Output
-import pytesseract
 import argparse
-import imutils
 import cv2
+import numpy as np
+import easyocr
+from pprint import pprint
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -11,10 +11,9 @@ ap.add_argument("-i", "--image", required=True,
 	help="path to input image to be OCR'd")
 args = vars(ap.parse_args())
 
-# load the input image, convert it from BGR to RGB channel ordering,
-# and use Tesseract to determine the text orientation
-image = cv2.imread(args["image"])
-rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-string = pytesseract.image_to_string(image)
+# ocr-ing
+reader = easyocr.Reader(['en'])
+result = reader.readtext(args["image"])
+
 # result
-print(string)
+pprint(result)
