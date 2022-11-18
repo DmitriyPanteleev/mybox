11186cde12a8fabe02a69c52a1ab6e75df4bee4c
Dmitriy Panteleev <dpanteleev@tradingview.com>
2022-11-18 14:18:25 +0300

    add fib shell

---
+++ fibbonaci/fib_trivial.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Enter the n number you want : "
+read n
+echo
+i=0
+j=1
+sum=0
+echo "$i"
+echo "$j"
+while [ $i -le $n ]
+do
+      sum=$(( $i + $j ))
+        i=$j
+    j=$sum
+    echo "$sum"
+done
