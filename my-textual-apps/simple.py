5323a4691804febfb116acad6dc5450f6a481a4e
Dmitriy Panteleev <dpanteleev@tradingview.com>
2023-05-23 09:56:47 +0300

    studing

--- my-textual-apps/simple.py
+++ my-textual-apps/simple.py
@@ -1,4 +1,5 @@
 from textual.app import App, ComposeResult
+from textual.containers import Horizontal, Vertical
 from textual.widgets import Static
 
 
@@ -6,10 +7,18 @@ class LayoutExample(App):
     CSS_PATH = "simple.css"
 
     def compose(self) -> ComposeResult:
-        yield Static("One", classes="box")
-        yield Static("Two", classes="box")
-        yield Static("Three", classes="box")
-
+        yield Horizontal(
+            Vertical(
+                Static("One", classes="box"),
+                Static("Two", classes="box"),
+                classes="column",
+            ),
+            Vertical(
+                Static("Three"),
+                Static("Four"),
+                classes="column",
+            ),
+        )
 
 if __name__ == "__main__":
     app = LayoutExample()
