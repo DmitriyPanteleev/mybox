9648a12dbc98f85ac21d9e132c07c01892af423d
Dmitriy Panteleev <dpanteleev@tradingview.com>
2023-06-26 17:34:28 +0300

    vertical layout

--- my-textual-apps/simple.py
+++ my-textual-apps/simple.py
@@ -11,9 +11,6 @@ class LayoutExample(App):
         yield Static("One", classes="box")
         yield Static("Two", classes="box")
         yield Static("Three", classes="box")
-        yield Static("Four", classes="box")
-        yield Static("Five", classes="box")
-        yield Static("Six", classes="box")
 
 if __name__ == "__main__":
     app = LayoutExample()
