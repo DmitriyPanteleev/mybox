0836442487fff7c2856ebf25e66307316dd16a79
Dmitriy Panteleev <dpanteleev@tradingview.com>
2023-06-29 17:38:41 +0300

    simple grid 3x2

--- my-textual-apps/simple.py
+++ my-textual-apps/simple.py
@@ -11,6 +11,9 @@ class LayoutExample(App):
         yield Static("One", classes="box")
         yield Static("Two", classes="box")
         yield Static("Three", classes="box")
+        yield Static("Four", classes="box")
+        yield Static("Five", classes="box")
+        yield Static("Six", classes="box")
 
 if __name__ == "__main__":
     app = LayoutExample()
