ae9ce242687e13b6f39e4416865e17f313b3a8d1
Dmitriy Panteleev <dpanteleev@tradingview.com>
2023-11-17 17:55:14 +0300

    Update interface.py with Text widget

--- my-textual-apps/interface.py
+++ my-textual-apps/interface.py
@@ -1,26 +1,9 @@
-import os
-from textual.app import App, ComposeResult
-from textual.containers import Container
-from textual.widgets import Static
+from textual import Text
 
-def get_terminal_size():
-    rows, columns = os.popen('stty size', 'r').read().split()
-    return int(rows), int(columns)
+text = Text(width=100, height=200)
 
-class MyApp(App):
-    CSS_PATH = "simple.css"
+text.rounded(radius=50)
 
-    def compose(self) -> ComposeResult:
-        rows, columns = get_terminal_size()
-        yield Container(
-            Static("Hello, World!", classes="panel"),
-            classes="panel-container",
-        )
+text.fill = "green"
 
-    def on_mount(self) -> None:
-        self.query_one(".panel-container").styles.width = str(get_terminal_size()[1])
-        self.query_one(".panel-container").styles.height = str(int(get_terminal_size()[0] * 2/3))
-
-if __name__ == "__main__":
-    app = MyApp()
-    app.run()
+text.render()
