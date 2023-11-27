af8171c6cb3f54852103bff121c244586bb76ba1
Dmitriy Panteleev <dpanteleev@tradingview.com>
2023-11-27 11:57:56 +0300

    Refactor interface.py to use Textual's App and Widget classes

--- my-textual-apps/interface.py
+++ my-textual-apps/interface.py
@@ -1,9 +1,24 @@
-from textual import Text
+from textual.app import App
+from textual.widget import Widget
 
-text = Text(width=100, height=200)
+class FullWidthWidget(Widget):
+    def render(self):
+        return self.style(f"{' ' * self.size[0]}", "on blue")
 
-text.rounded(radius=50)
+class MyApp(App):
+    async def on_mount(self) -> None:
+        # Get the dimensions of the screen
+        width, height = self.view.dimensions
 
-text.fill = "green"
+        # Calculate the height for the widget
+        widget_height = int(2/3 * height)
 
-text.render()
+        # Create a widget with the calculated dimensions
+        widget = FullWidthWidget(width=width, height=widget_height)
+
+        # Add the widget to the root view
+        await self.view.dock(widget)
+
+app = MyApp()  # Create an instance of MyApp
+app.run()  # Run the instance, not the class
+
