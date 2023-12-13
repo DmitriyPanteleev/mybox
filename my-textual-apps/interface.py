74d9a3f1ef44da40941edafdd8d29417a0185ddc
Dmitriy Panteleev <dpanteleev@tradingview.com>
2023-12-13 16:29:17 +0300

    Add CenterApp class and CSS styling for centering elements

--- my-textual-apps/interface.py
+++ my-textual-apps/interface.py
@@ -1,24 +1,36 @@
-from textual.app import App
-from textual.widget import Widget
+from textual.app import App, ComposeResult
+from textual.widgets import Static
 
-class FullWidthWidget(Widget):
-    def render(self):
-        return self.style(f"{' ' * self.size[0]}", "on blue")
 
-class MyApp(App):
-    async def on_mount(self) -> None:
-        # Get the dimensions of the screen
-        width, height = self.view.dimensions
+class CenterApp(App):
+    """How to center things."""
 
-        # Calculate the height for the widget
-        widget_height = int(2/3 * height)
+    CSS = """
+    Screen {
+        align: center top;
+    }
 
-        # Create a widget with the calculated dimensions
-        widget = FullWidthWidget(width=width, height=widget_height)
+    #map {
+        border: round white;
+        text-align: center;
+        content-align: center middle;
+        width: 100%;
+        height: 70%;
+    }
 
-        # Add the widget to the root view
-        await self.view.dock(widget)
+    #params {
+        border: round white;
+        text-align: center;
+        content-align: center middle;
+        width: 100%;
+        height: 30%;
+    }
+    """
 
-app = MyApp()  # Create an instance of MyApp
-app.run()  # Run the instance, not the class
+    def compose(self) -> ComposeResult:
+        yield Static("Hero!", id="map")
+        yield Static("Parameters", id="params")
 
+if __name__ == "__main__":
+    app = CenterApp()
+    app.run()
