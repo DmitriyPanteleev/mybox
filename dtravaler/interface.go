6652a3172a4e2e45dc7d3005275d3d23851c697c
Dmitriy Panteleev <dpanteleev@tradingview.com>
2023-12-13 17:45:57 +0300

    Refactor interface styling and add custom top border

--- dtravaler/interface.go
+++ dtravaler/interface.go
@@ -15,21 +15,23 @@ func main() {
 		fmt.Println("Error getting terminal size:", err)
 		return
 	}
-	fmt.Println("Width:", width, "Height:", height)
 
 	// Define the height as 2/3 of the terminal height
 	boxHeight := (2 * (height - 1)) / 3
 
+	// Create a custom top border with the title
+	borderTop := " map " + lipgloss.NewStyle().Border(lipgloss.RoundedBorder()).Render("")[:width-5]
+
 	// Create a lip gloss style with the desired dimensions and border
 	style := lipgloss.NewStyle().
-		Width(width - 2).                // Set to full terminal width
-		Height(boxHeight).               // Set to 2/3 of terminal height
-		Border(lipgloss.RoundedBorder()) // Use rounded corners
+		Width(width - 2).                 // Set to full terminal width
+		Height(boxHeight).                // Set to 2/3 of terminal height
+		Border(lipgloss.RoundedBorder()). // Use rounded corners
+		BorderTop(borderTop)              // Set the custom top border
 
 	// Draw the box
 	box := style.Render("")
 
 	// Print the box to the terminal
 	fmt.Println(box)
-	fmt.Println("Width:", width, "Height:", boxHeight)
 }
