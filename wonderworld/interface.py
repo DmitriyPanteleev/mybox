63562a253961ab9df770e7e868593e0ce9e32913
Dmitriy Panteleev <dpanteleev@tradingview.com>
2023-12-15 18:15:35 +0300

    create wonderworld

---
+++ wonderworld/interface.py
@@ -0,0 +1,23 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Static
+
+class GameApp(App):
+    """Wonderworld game interface."""
+
+    CSS_PATH = "interface.css"
+
+    def compose(self) -> ComposeResult:
+        mapscreen = Static(id="map")
+        mapscreen.border_title = "Map"
+        paramscreen = Static(id="params")
+        paramscreen.border_title = "Parameters"
+        yield mapscreen
+        yield paramscreen
+
+    def on_key(self, event):
+        if event.key == "q":
+            self.exit()
+
+if __name__ == "__main__":
+    app = GameApp()
+    app.run()
