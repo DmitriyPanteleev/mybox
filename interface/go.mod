5180bfb4f37905284bcd0deb39b5a306da00634f
Dmitriy Panteleev <pant-dm@yandex.ru>
2024-08-24 17:29:05 +0300

    test

--- interface/go.mod
+++ interface/go.mod
@@ -3,13 +3,6 @@ module mybox/interface
 go 1.22.6
 
 require (
-	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
-	github.com/charmbracelet/lipgloss v0.13.0 // indirect
-	github.com/charmbracelet/x/ansi v0.1.4 // indirect
-	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/muesli/termenv v0.15.2 // indirect
-	github.com/rivo/uniseg v0.4.7 // indirect
-	golang.org/x/sys v0.19.0 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
 )
--- interface/interface.go
+++ interface/interface.go
@@ -2,38 +2,69 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/charmbracelet/lipgloss"
+	"github.com/nsf/termbox-go"
 )
 
+func drawRoundedBox(x, y, w, h int, title string) {
+	// Верхняя часть рамки
+	termbox.SetCell(x, y, '/', termbox.ColorDefault, termbox.ColorDefault)
+	for i := 1; i < w-1; i++ {
+		termbox.SetCell(x+i, y, '-', termbox.ColorDefault, termbox.ColorDefault)
+	}
+	termbox.SetCell(x+w-1, y, '\\', termbox.ColorDefault, termbox.ColorDefault)
+
+	// Нижняя часть рамки
+	termbox.SetCell(x, y+h-1, '\\', termbox.ColorDefault, termbox.ColorDefault)
+	for i := 1; i < w-1; i++ {
+		termbox.SetCell(x+i, y+h-1, '-', termbox.ColorDefault, termbox.ColorDefault)
+	}
+	termbox.SetCell(x+w-1, y+h-1, '/', termbox.ColorDefault, termbox.ColorDefault)
+
+	// Левая и правая части рамки
+	for i := 1; i < h-1; i++ {
+		termbox.SetCell(x, y+i, '|', termbox.ColorDefault, termbox.ColorDefault)
+		termbox.SetCell(x+w-1, y+i, '|', termbox.ColorDefault, termbox.ColorDefault)
+	}
+
+	// Заголовок
+	for i, ch := range title {
+		termbox.SetCell(x+2+i, y, ch, termbox.ColorDefault, termbox.ColorDefault)
+	}
+}
+
+func drawText(x, y int, text string) {
+	for i, ch := range text {
+		termbox.SetCell(x+i, y, ch, termbox.ColorDefault, termbox.ColorDefault)
+	}
+}
+
 func main() {
-	// Определяем стили для рамок и текста
-	borderStyle := lipgloss.NewStyle().
-		Border(lipgloss.NormalBorder()).
-		BorderForeground(lipgloss.Color("63")).
-		Padding(1, 2).
-		Width(20)
+	err := termbox.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer termbox.Close()
 
-	titleStyle := lipgloss.NewStyle().
-		Bold(true).
-		Foreground(lipgloss.Color("205"))
+	// Рисуем первую рамку
+	drawRoundedBox(1, 1, 40, 10, "Map")
+	drawText(20, 5, "*")
+	drawText(19, 4, "|")
+	drawText(19, 6, "|")
+	drawText(18, 5, "-")
+	drawText(20, 5, "*")
+	drawText(21, 5, "-")
+	drawText(20, 3, "*")
+	drawText(20, 7, "*")
 
-	textStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("240"))
+	// Рисуем вторую рамку
+	drawRoundedBox(1, 12, 20, 5, "Inv")
+	drawText(3, 14, "Str: 5")
+	drawText(3, 15, "Int: 7")
 
-	// Создаем текст с заголовком "Map"
-	mapHeader := titleStyle.Render("Map")
-	mapContent := textStyle.Render("\n  *\n * *\n  *")
-	mapBox := borderStyle.Render(mapContent)
-	mapWithHeader := lipgloss.PlaceHorizontal(len(mapBox), lipgloss.Left, mapHeader) + "\n" + mapBox
+	termbox.Flush()
 
-	// Создаем текст с заголовком "Inv"
-	invHeader := titleStyle.Render("Inv")
-	invContent := textStyle.Render("Str: 5\nInt: 7")
-	invBox := borderStyle.Render(invContent)
-	invWithHeader := lipgloss.PlaceHorizontal(len(invBox), lipgloss.Right, invHeader) + "\n" + invBox
-
-	// Выводим рамки в консоль
-	fmt.Println(mapWithHeader)
-	fmt.Println(invWithHeader)
+	fmt.Println("\n Press any key to exit...")
+	termbox.PollEvent()
 }
